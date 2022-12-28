f5d5b017e0d7199d4daad2e4c87aa4abb6455fd4
daejinious <size_chi@naver.com>
2022-12-28 17:56:49 +0900

    [2022-12-28] 기본 상태로 시작

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
-// 한글 주석처리a}
-// 0 abcdef
-export class AppModule {
-  /**
-   *
-   * @returns string
-   */
-  findAll(): string {
-    const a = {};
-    const b = 2;
-    if (a === b) {
-      console.log(`test a = ${a}`);
-    } else {
-      console.log(`test a = ${a}`);
-    }
-    return 'test';
-  }
-}
+export class AppModule {}
