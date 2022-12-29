08e2e9ef01571887cc607d5ae2ac049b209208e1
daejinious <size_chi@naver.com>
2022-12-29 14:03:15 +0900

    Validator pipeline 적용

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
+import CreateMovieDto from './dto/create-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -26,17 +27,17 @@ export class MoviesController {
   }
 
   @Get('/:id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
     return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData: any) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
